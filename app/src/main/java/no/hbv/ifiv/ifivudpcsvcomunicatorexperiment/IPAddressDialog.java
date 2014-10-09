753e1fe76285b9f420f0f76c7874f44e27fc0aab
Rune Langoy <rune.langoy@gmail.com>
2014-10-09 12:44:22 +0200

    Added IP text filter

--- app/src/main/java/no/hbv/ifiv/ifivudpcsvcomunicatorexperiment/IPAddressDialog.java
+++ app/src/main/java/no/hbv/ifiv/ifivudpcsvcomunicatorexperiment/IPAddressDialog.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
@@ -86,6 +88,40 @@ public class IPAddressDialog extends AlertDialog.Builder
     }
 
 
+    // Cofigures IP Filtter
+    private void ipTextFilter(TextView textviewToFormat)
+    {
+
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start,
+                                       int end, Spanned dest, int dstart, int dend) {
+                if (end > start) {
+                    String destTxt = dest.toString();
+                    String resultingTxt = destTxt.substring(0, dstart) +
+                            source.subSequence(start, end) +
+                            destTxt.substring(dend);
+                    if (!resultingTxt.matches ("^\\d{1,3}(\\." +
+                            "(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
+                        return "";
+                    } else {
+                        String[] splits = resultingTxt.split("\\.");
+                        for (int i=0; i<splits.length; i++) {
+                            if (Integer.valueOf(splits[i]) > 255) {
+                                return "";
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+        };
+
+        textviewToFormat.setFilters(filters);
+
+    }
+
     public IPAddressDialog(final Context context)
     {
         super(context);
@@ -94,6 +130,7 @@ public class IPAddressDialog extends AlertDialog.Builder
         View dlgView = inflater.inflate(R.layout.dialog_ipaddress, null);
         this.setView(dlgView);
         mIPAddress = (TextView) dlgView.findViewById(R.id.ipAddress);
+        ipTextFilter(mIPAddress);
         mIPPort= (TextView) dlgView.findViewById(R.id.ipPort);
         this.setTitle("IP Configuration");
         final IPAddressDialog thisDialog=this;
