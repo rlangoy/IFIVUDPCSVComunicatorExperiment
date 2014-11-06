2a50cafa17e80db31e8847acb77925a178115e54
Rune Langoy <rune.langoy@gmail.com>
2014-11-06 11:07:04 +0100

    Added LineChart data (need to fix redraw of the chart)

--- app/src/main/java/no/hbv/ifiv/ifivudpcsvcomunicatorexperiment/UDPGraphingFragment.java
+++ app/src/main/java/no/hbv/ifiv/ifivudpcsvcomunicatorexperiment/UDPGraphingFragment.java
@@ -5,9 +5,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarLineChartBase;
 import com.github.mikephil.charting.charts.LineChart;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 
 /**
@@ -25,6 +32,22 @@ public class UDPGraphingFragment extends SherlockFragment {
 
         mChart = (LineChart) rootView.findViewById(R.id.chart1);
 
+        // if enabled, the chart will always start at zero on the y-axis
+        mChart.setStartAtZero(false);
+
+        // disable the drawing of values into the chart
+        mChart.setDrawYValues(false);
+        mChart.setDrawBorder(true);
+        mChart.setBorderPositions(new BarLineChartBase.BorderPosition[] {
+                BarLineChartBase.BorderPosition.BOTTOM
+        });
+
+       // no description text
+        mChart.setDescription("$GARPH,values");
+        mChart.setNoDataTextDescription("No $GARPH,values received");
+
+
+
         return rootView;
     }
 
@@ -36,6 +59,33 @@ public class UDPGraphingFragment extends SherlockFragment {
         if (csvMessage[0].equalsIgnoreCase("$graph")==true)
         {
             Log.d("UDPGraphingFragment", "equalsIgnoreCase(\"$graph\")==true");
+
+            int count=360;
+
+            ArrayList<String> xVals = new ArrayList<String>();
+            ArrayList<Entry> yVals = new ArrayList<Entry>();
+            for (int i = 0; i < count; i++) {
+                xVals.add((i) + "");
+
+                float val = (float) Math.sin(2*Math.PI*i/count);
+                yVals.add(new Entry(val, i));
+
+            }
+
+            // create a dataset and give it a type
+            LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+
+            ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData data = new LineData(xVals, dataSets);
+
+            // set data
+            mChart.setData(data);
+
+            // redraw
+            mChart.invalidate();
         }
 
     }
